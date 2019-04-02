45189f934606412242eb2d2c7c4d4b8ad71d74aa
hakpower <hakpower9592303@gmail.com>
2019-04-02 19:32:24 +0300

    Merge branch 'master' of C:\Users\Wonhak\AndroidStudioProjects\-Android-SafetyBallTimer_Server_ with conflicts.

--- app/src/main/java/com/ssarl/sbtformanager/ListviewActivity.java
+++ app/src/main/java/com/ssarl/sbtformanager/ListviewActivity.java
@@ -61,42 +61,29 @@ public class ListviewActivity extends BaseAdapter {
             LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(layout, parent, false);
         }
-        int sumArr[]=new int[20];
-        int count[]=new int [20];
-        for(int j=0; j<20;j++)
-        {  count[j]=1;
-            for(int k=0; k<answers.size();k++)
-            {
-                if(j==Integer.parseInt(answers.get(k).questionNum)){
-                    sumArr[j]=sumArr[j]+Integer.parseInt(answers.get(k).value);
-                    count[j]=count[j]+1;
-                }
-            }
-            Log.d("카운트",Integer.toString(count[j]));
-        }
 
-
+        Log.d("되나안되나", answers.get(0).sentTime);
         GraphView graph = (GraphView) convertView.findViewById(R.id.graph);
 
-        dataPoints = new DataPoint[20];
-        for(int i=0; i<20; i++){
-            int xPos = i;
-            int yPos = sumArr[i];
+        dataPoints = new DataPoint[answers.size()];
+        for(int i=0; i<answers.size(); i++){
+            double xPos = Double.parseDouble((answers.get(i).sentTime));
+            int yPos = Integer.parseInt(answers.get(i).value);
             dataPoints[i] = new DataPoint(xPos, yPos);
-            Log.d("문제번호"+i,Integer.toString(xPos));
-            Log.d("합산"+i,Integer.toString(yPos));
+            Log.d("안되나",Double.toString(xPos));
+
 
         }
 
         //PointsGraphSeries<DataPoint>series=new PointsGraphSeries<DataPoint>(dataPoints);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(dataPoints);
-       graph.getViewport().setMinX(1);
-       graph.getViewport().setMaxX(20);
-       graph.getViewport().setMinY(1);
-        graph.getViewport().setMaxY(100);
-       graph.getViewport().setXAxisBoundsManual(true);
-        graph.getViewport().setYAxisBoundsManual(true);
-       //series.setCustomPaint(paint);
+     // graph.getViewport().setMinX(1);
+      // graph.getViewport().setMaxX(24);
+      //  graph.getViewport().setMinY(1);
+      //  graph.getViewport().setMaxY(100);
+       // graph.getViewport().setXAxisBoundsManual(true);
+       // graph.getViewport().setYAxisBoundsManual(true);
+        //series.setCustomPaint(paint);
         series.setColor(Color.BLUE);
         graph.addSeries(series);
 
